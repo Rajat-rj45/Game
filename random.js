4641d824c670a81b247838776ea07f01b9e63705
Rajat-rj45 <rajatsuryavanshi404@gmail.com>
2025-11-08 10:48:29 +0530

    m

--- random.js
+++ random.js
@@ -1,41 +1,14 @@
 const readline = require("readline");
+const http= require('http')
+const fs=require('fs')
 
-// Create input interface
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const a = Math.floor(Math.random() * 100); // random number 0–99
-let count = 1;
-
-console.log("Guess the number between 0 and 100!");
-
-function ask() {
-  rl.question("Enter your guess: ", (input) => {
-    const b = parseInt(input);
-
-    if (isNaN(b)) {
-      console.log("Please enter a valid number!");
-      return ask();
+http.createServer((req,res)=>{
+  fs.readFile("game.html", "utf-8", (err,data)=>{
+    if(err){
+      res.write("internal error");
+    }else{
+      res.end(data)
     }
+  })
+}).listen(3000);
 
-    if (a === b) {
-      console.log("🎉 You won!");
-      console.log("Total tries = " + count);
-      rl.close();
-    } else if (a > b) {
-      console.log("Guess a bigger number");
-      console.log("Try again!");
-      count++;
-      ask();
-    } else if (a < b) {
-      console.log("Guess a smaller number");
-      console.log("Try again!");
-      count++;
-      ask();
-    }
-  });
-}
-
-ask();
